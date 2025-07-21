121868a281ba6f846b5e74e5fe145694c251c311
Shivam Kumar <shivamkumar776499@gmail.com>
2025-07-21 15:10:13 +0530

    add url

--- fronted/src/api/axios.tsx
+++ fronted/src/api/axios.tsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: 'https://doctorappointmentbackend-ilmx.onrender.com/',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
